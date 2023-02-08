485db3c1614041a1aa92c7dde586d49fee49e9a0
孤之界 <f20001012@gmail.com>
2023-02-08 10:06:51 +0800

    misc: 更新 Log 類別

--- Youtube Stream Record/Log.cs
+++ Youtube Stream Record/Log.cs
@@ -1,34 +1,65 @@
 ﻿using System;
+using System.Diagnostics;
+
 public static class Log
 {
+    static object _lock = new object();
+
+    enum LogType { Verb, Stream, Info, Warn, Error }
+
     public static void Info(string text, bool newLine = true)
     {
-        FormatColorWrite(text, ConsoleColor.DarkYellow, newLine);
+        lock (_lock)
+        {
+            FormatColorWrite(text, ConsoleColor.DarkYellow, newLine);
+        }
     }
 
     public static void Warn(string text, bool newLine = true)
     {
-        FormatColorWrite(text, ConsoleColor.DarkMagenta, newLine);
+        lock (_lock)
+        {
+            FormatColorWrite(text, ConsoleColor.DarkMagenta, newLine);
+        }
     }
 
     public static void Error(string text, bool newLine = true)
     {
-        FormatColorWrite(text, ConsoleColor.DarkRed, newLine);
+        lock (_lock)
+        {
+            FormatColorWrite(text, ConsoleColor.DarkRed, newLine);
+        }
     }
 
     public static void Debug(string text, bool newLine = true)
     {
-#if DEBUG
-        FormatColorWrite($"[Debug] {text}", ConsoleColor.Green, newLine);
-#endif
+        if (!Debugger.IsAttached)
+            return;
+
+        lock (_lock)
+        {
+            FormatColorWrite(text, ConsoleColor.Cyan, newLine);
+        }
+    }
+
+    public static void Error(Exception ex, string text, bool newLine = true)
+    {
+        lock (_lock)
+        {
+            FormatColorWrite(text, ConsoleColor.DarkRed, newLine);
+            FormatColorWrite(ex.ToString(), ConsoleColor.DarkRed, newLine);
+        }
     }
 
     public static void FormatColorWrite(string text, ConsoleColor consoleColor = ConsoleColor.Gray, bool newLine = true)
     {
-        text = DateTime.Now.ToString() + " " + text;
-        Console.ForegroundColor = consoleColor;
-        if (newLine) Console.WriteLine(text);
-        else Console.Write(text);
-        Console.ForegroundColor = ConsoleColor.Gray;
+        lock (_lock)
+        {
+            text = $"[{DateTime.Now}] {text}";
+            Console.ForegroundColor = consoleColor;
+            if (newLine) Console.WriteLine(text);
+            else Console.Write(text);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
     }
 }
